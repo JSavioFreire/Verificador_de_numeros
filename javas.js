530692cc9579e310129e7e41d2c36d706d758865
Sávio Freire <freiresavio7@gmail.com>
2022-08-15 11:04:50 -0300

    Update javas.js

--- javas.js
+++ javas.js
@@ -3,12 +3,14 @@ var btadd = document.querySelector('#btadd')
 var tab = document.querySelector('#tab')
 btadd.addEventListener('click', clicouadd(localnum))
 
+var teste = document.querySelector('#teste')
+
 function clicouadd(n){
     for(var posi = 0; posi < localnum.length; posi++){
         localnum[posi] = document.querySelector('#number')
         teste.innerHTML = localnum[posi]
+        return localnum[posi]
+        
     }
 
 }
-var teste = document.querySelector('#teste')
-teste.innerHTML = localnum.length
